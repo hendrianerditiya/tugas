039d9ed1ddf9eb28340693839c826612112d54f0
hendrianerditiya <hendrianerditya@gmail.com>
2015-10-13 16:43:52 +0700

    Tugas PBO
    Tugas

---
+++ paket/src/jangan.java
@@ -0,0 +1,31 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class jangan {
+    void tolongjangan(){
+        JOptionPane.showMessageDialog(null,"woii, sudah di bilang jangan!");
+    }
+    void maafjangan(){
+    JOptionPane.showMessageDialog(null, "maaf jangan di tekan");
+    }
+    void tombolrusak(){
+    JOptionPane.showMessageDialog(null,"sudah di bilang tombol rusak malah di tekan");
+    }
+    void cobalain(){
+    JOptionPane.showMessageDialog(null, "maaf tombol tidak berfungsi");
+    }
+    void cobalagi() {
+        JOptionPane.showMessageDialog(null,"anda tidak beruntung" );
+    }
+   
+}
